0b1b216842bc229e41016ac771acbb165269bb53
Justin6Liu <114266776+Justin6Liu@users.noreply.github.com>
2026-04-14 00:09:24 -0400

    Switch to box-only cascade config to avoid missing masks; use box COCO weights

--- configs/cascade_omr.py
+++ configs/cascade_omr.py
@@ -2,8 +2,8 @@
 # Adjust paths below for your environment.
 
 _base_ = [
-    # Use the COCO-pretrained Cascade Mask R-CNN R50 FPN weights
-    "mmdet::cascade_rcnn/cascade-mask-rcnn_r50_fpn_1x_coco.py",
+    # Use Cascade R-CNN backbone config (box only; our dataset lacks masks)
+    "mmdet::cascade_rcnn/cascade-rcnn_r50_fpn_1x_coco.py",
 ]
 
 data_root = "/home/users/jl1430/jl1430/OMR-training/datasets/muscima_coco/"
@@ -51,11 +51,6 @@ model = dict(
             dict(type="Shared2FCBBoxHead", num_classes=115),
             dict(type="Shared2FCBBoxHead", num_classes=115),
         ],
-        mask_head=[
-            dict(type="FCNMaskHead", num_classes=115),
-            dict(type="FCNMaskHead", num_classes=115),
-            dict(type="FCNMaskHead", num_classes=115),
-        ],
     )
 )
 
@@ -68,4 +63,4 @@ train_cfg = dict(max_epochs=12)
 
 default_hooks = dict(checkpoint=dict(type="CheckpointHook", interval=1, max_keep_ckpts=1))
 
-load_from = "/home/users/jl1430/jl1430/OMR-training/cascade_mask_rcnn_r50_fpn_1x_coco_20200203-9d4dcb24.pth"
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco/cascade_rcnn_r50_fpn_1x_coco_20200317-0b6a2fbf.pth"
