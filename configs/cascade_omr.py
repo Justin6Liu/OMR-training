f5b39c6a435e729b6b7473b0f4cfc44cc52918a8
Justin6Liu <114266776+Justin6Liu@users.noreply.github.com>
2026-04-14 01:00:33 -0400

    Fix cascade COCO checkpoint URL (9d4dcb4 vs 0b6a2fb4)

--- configs/cascade_omr.py
+++ configs/cascade_omr.py
@@ -68,4 +68,4 @@ train_cfg = dict(max_epochs=12)
 
 default_hooks = dict(checkpoint=dict(type="CheckpointHook", interval=1, max_keep_ckpts=1))
 
-load_from = "https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco/cascade_rcnn_r50_fpn_1x_coco_20200317-0b6a2fbf.pth"
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn/cascade_rcnn_r50_fpn_1x_coco/cascade_rcnn_r50_fpn_1x_coco_20200317-0b6a2fb4.pth"
